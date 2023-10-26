abd65c757d74a2823193e7fd485aacddadd9bee1
TANVI BANSAL <126918761+TanviBansal29@users.noreply.github.com>
2023-10-26 18:25:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def prompt_add_book():
 def list_books():
     books = database.get_all_books()
     for book in books:
-        read= 'YES' if book['read'] else 'NO'
-        print(f"{book['name']} by {book['author']},read:{read}")
+        read = 'YES' if book['read'] == '1' else 'NO'
+        print(f"{book['name']} by {book['author']}, read: {book['read']}")
 
 
 def prompt_read_book():
